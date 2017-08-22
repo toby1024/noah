0a2158a4a01e76a3883d7468f59088696fb6c642
张斌 <zhangbin@skio.cn>
2017-08-22 11:29:26 +0800

    add eureka dockerfile

---
+++ gateway/src/main/java/cn/skio/gateway/filter/post/PostFilter.groovy
@@ -0,0 +1,36 @@
+package cn.skio.gateway.filter.post
+
+import com.netflix.zuul.ZuulFilter
+import com.netflix.zuul.context.RequestContext
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import javax.servlet.http.HttpServletResponse
+
+class PostFilter extends ZuulFilter {
+
+    Logger log = LoggerFactory.getLogger(PostFilter.class)
+
+    @Override
+    String filterType() {
+        return "post"
+    }
+
+    @Override
+    int filterOrder() {
+        return 2000
+    }
+
+    @Override
+    boolean shouldFilter() {
+        return true
+    }
+
+    @Override
+    Object run() {
+        log.info("this is a post filter: Receive response")
+        HttpServletResponse response = RequestContext.getCurrentContext().getResponse()
+        response.getOutputStream().print(", i am filter")
+        response.flushBuffer() nz
+    }
+}
