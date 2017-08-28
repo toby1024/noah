f68df26e74dead53407956cc97d8aceb9c81327b
张斌 <zhangbin@skio.cn>
2017-08-28 17:30:25 +0800

    完善redis

--- gateway/src/main/java/cn/skio/gateway/auth/service/AuthService.java
+++ gateway/src/main/java/cn/skio/gateway/auth/service/AuthService.java
@@ -7,6 +7,7 @@ import cn.skio.gateway.auth.jwt.JwtUser;
 import cn.skio.gateway.auth.jwt.JwtUserFactory;
 import cn.skio.gateway.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -21,6 +22,12 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class AuthService {
+
+    private static final String USER_CATCH_PREFIX = "user_";
+    private static final String TOKEN_CATCH_PREFIX = "token_";
+
+    @Value("${redis.catch_time_out_days}")
+    private int catch_time_out;
 
     @Autowired
     private UserRepository userRepository;
@@ -38,19 +45,29 @@ public class AuthService {
     private JwtTokenUtil jwtTokenUtil;
 
     public String login(String username, String password) {
-        ValueOperations<String, JwtUser> valueOperations = redisTemplate.opsForValue();
-        JwtUser jwtUer = null;
+        ValueOperations<String, JwtUser> jwtUserCatch = redisTemplate.opsForValue();
+        ValueOperations<String, String> tokenCatch = redisTemplate.opsForValue();
 
-        if(redisTemplate.hasKey("user_" + username)){
-            jwtUer = valueOperations.get("user_" + username);
+        JwtUser jwtUser = null;
+        String token = null;
+
+        if(redisTemplate.hasKey(USER_CATCH_PREFIX + username) && (jwtUser = jwtUserCatch.get(USER_CATCH_PREFIX + username)) != null){
+            token = tokenCatch.get(TOKEN_CATCH_PREFIX + jwtUser.getUuid());
+            if(token == null){
+                token = jwtTokenUtil.generateToken(jwtUser);
+                tokenCatch.set(TOKEN_CATCH_PREFIX + jwtUser.getUuid(), token, catch_time_out, TimeUnit.DAYS);
+            }
         } else {
             User user = userRepository.findByUsername(username);
-            jwtUer = JwtUserFactory.create(user);
-            valueOperations.set("user_" + username, jwtUer, 10, TimeUnit.DAYS);
+            jwtUser = JwtUserFactory.create(user);
+            jwtUserCatch.set(USER_CATCH_PREFIX + username, jwtUser, catch_time_out, TimeUnit.DAYS);
+
+            token = jwtTokenUtil.generateToken(jwtUser);
+            tokenCatch.set(TOKEN_CATCH_PREFIX + jwtUser.getUuid(), token, catch_time_out, TimeUnit.DAYS);
         }
 
-        if(jwtUer != null && new BCryptPasswordEncoder().matches(password, jwtUer.getPassword()) && jwtUer.isActive()) {
-            return jwtTokenUtil.generateToken(jwtUer);
+        if(jwtUser != null && new BCryptPasswordEncoder().matches(password, jwtUser.getPassword()) && jwtUser.isActive()) {
+            return token;
         }else{
             return null;
         }
@@ -67,11 +84,15 @@ public class AuthService {
         userToAdd.setUuid(UUID.randomUUID().toString().replaceAll("-",""));
         User userAdded = userRepository.save(userToAdd);
 
-        ValueOperations<String, JwtUser> valueOperations = redisTemplate.opsForValue();
-        JwtUser jwtUer = JwtUserFactory.create(userAdded);
-        valueOperations.set("user_" + username, jwtUer, 10, TimeUnit.DAYS);
+        ValueOperations<String, JwtUser> jwtUserCatch = redisTemplate.opsForValue();
+        JwtUser jwtUser = JwtUserFactory.create(userAdded);
+        jwtUserCatch.set(USER_CATCH_PREFIX + username, jwtUser, catch_time_out, TimeUnit.DAYS);
 
-        return jwtTokenUtil.generateToken(jwtUer);
+        String token = jwtTokenUtil.generateToken(jwtUser);
+        ValueOperations<String, String> tokenCatch = redisTemplate.opsForValue();
+        tokenCatch.set(TOKEN_CATCH_PREFIX + jwtUser.getUuid(), token, catch_time_out, TimeUnit.DAYS);
+
+        return token;
     }
 
     public List<User> activeUsers(int pageNo, int pageSize) {
